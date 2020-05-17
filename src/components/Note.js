655e3f1984d7934b027db9a7e14774e6900869c5
canyener <can.yener@hotmail.com.tr>
2020-05-18 00:24:43 +0300

    Adds context to Note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
-const Note = ({ note, removeNote}) => {
+import NotesContext from '../context/notes-context'
+
+const Note = ({ note }) => {
+  const { dispatch } = useContext(NotesContext) 
+  
   return (
     <div>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
-      <button onClick={() => removeNote(note.title)}>x</button>
+      <button onClick={() => dispatch({ type: 'REMOVE_NOTE', title: note.title })}>x</button>
     </div>
   )
 }
