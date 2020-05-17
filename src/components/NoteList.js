d9f16ddc40fa21ee2d080481de1dc6c55367c1c3
canyener <can.yener@hotmail.com.tr>
2020-05-17 22:52:52 +0300

    Moves note list to its own component

---
+++ src/components/NoteList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Note from './Note'
+
+const NoteList = ({ notes, removeNote }) => {
+   return notes.map(note => (
+      <Note key={note.title} note={note} removeNote={removeNote} />
+    ))
+}
+
+export { NoteList as default }
