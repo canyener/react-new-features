ea23ec98258a0699a9915fa63f67f37d55dd5ae4
canyener <can.yener@hotmail.com.tr>
2020-05-17 23:01:01 +0300

    Adds AddNoteForm component

---
+++ src/components/AddNoteForm.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const AddNoteForm = ({ dispatch }) => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const addNote = (e) => {
+    e.preventDefault()
+
+    dispatch({ type: 'ADD_NOTE', title, body })
+    setTitle('')
+    setBody('')
+  }
+
+  return (
+    <div>
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} placeholder="title" onChange={e => setTitle(e.target.value)} />
+        <textarea value={body} placeholder="body" onChange={e => setBody(e.target.value)} />
+        <button>Add Note</button>
+      </form>
+    </div>
+  )
+}
+
+export { AddNoteForm as default }
