4eee99502676dab31903b5092f5f5de682b6105c
canyener <can.yener@hotmail.com.tr>
2020-05-18 00:25:45 +0300

    Adds context to NoteList component

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -4,11 +4,11 @@ import Note from './Note'
 
 import NotesContext from '../context/notes-context'
 
-const NoteList = ({ removeNote }) => {
+const NoteList = () => {
    const { notes } = useContext(NotesContext)
 
    return notes.map(note => (
-      <Note key={note.title} note={note} removeNote={removeNote} />
+      <Note key={note.title} note={note} />
     ))
 }
 
