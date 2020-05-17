d83399ae675dad82288197107959cdf651973a83
canyener <can.yener@hotmail.com.tr>
2020-05-17 23:01:17 +0300

    Imports AddNoteForm component

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,24 +1,15 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React, { useEffect, useReducer } from 'react'
 
 import notesReducer from '../reducers/notes'
 
 import NoteList from './NoteList'
+import AddNoteForm from './AddNoteForm'
 
 const NoteApp = () => {
 
   //1st parameter: reducer function.
   //2nd parameter: initial state.
   const [notes, dispatch] = useReducer(notesReducer, [])
-  const [title, setTitle] = useState('')
-  const [body, setBody] = useState('')
-  
-  const addNote = (e) => {
-    e.preventDefault()
-
-    dispatch({ type: 'ADD_NOTE', title, body })
-    setTitle('')
-    setBody('')
-  }
 
   const removeNote = (titleToRemove) => {
     dispatch({ type: 'REMOVE_NOTE', title: titleToRemove })
@@ -39,12 +30,7 @@ const NoteApp = () => {
     <div>
       <h1>Notes</h1>
       <NoteList notes={notes} removeNote={removeNote} />
-      <p>Add note</p>
-      <form onSubmit={addNote}>
-        <input value={title} placeholder="title" onChange={e => setTitle(e.target.value)} />
-        <textarea value={body} placeholder="body" onChange={e => setBody(e.target.value)} />
-        <button>Add Note</button>
-      </form>
+      <AddNoteForm dispatch={dispatch} />
     </div>
   )
 }
