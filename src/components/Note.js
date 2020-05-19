fb2b928e397116ea7894be44ac2057875b6e8191
canyener <can.yener@hotmail.com.tr>
2020-05-19 18:00:26 +0300

    Imports useMousePosition to Note component and uses it

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,14 +1,17 @@
 import React, { useContext } from 'react'
 
 import NotesContext from '../context/notes-context'
+import useMousePosition from '../hooks/useMousePosition'
 
 const Note = ({ note }) => {
   const { dispatch } = useContext(NotesContext) 
-  
+  const position = useMousePosition()
+
   return (
     <div>
       <h3>{note.title}</h3>
       <p>{note.body}</p>
+      <p>{position.x}, {position.y}</p>
       <button onClick={() => dispatch({ type: 'REMOVE_NOTE', title: note.title })}>x</button>
     </div>
   )
